54b487255890960b4d498f0dd38938a72b64d0e3
Somshekhar Menon <35287133+SOMSHEKHAR-MENON@users.noreply.github.com>
2022-05-27 16:17:29 +0530

    Time: 2 ms (29.81%), Space: 55.7 MB (34.32%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -2,19 +2,42 @@ class Solution {
     public void reverseString(char[] s) {
         
         //System.out.println(s);
-        
-        int left, right = s.length-1;
-        
-        for(left=0; left < right; left++,right--){
-            
-            char temp = s[left];
-            s[left]   = s[right];
-            s[right]  = temp;
-        }
-        
-        for(char c : s){
-            System.out.print(c);
-        }
+        int left=0,right=s.length-1;
+        reverse(s,left,right);
+        return ;
         
     }
+    
+    public void reverse(char[] s, int left,int right){
+        
+        if(left > right){
+            
+            //System.out.print(s);
+            return ;
+        }
+        
+        char temp = s[left];
+        s[left]   = s[right];
+        s[right]  = temp;
+        
+        reverse(s,left+1,right-1);
+    }   
 }
+
+/*  public void reverseString(char[] s) {
+         int i=0;
+         int j=s.length-1;
+         letMeReverse(s,i,j);
+            
+    }
+   void letMeReverse(char[] s, int i, int j){
+        if(i>j) return; // Base Case
+        char temp=s[i]; // Solving just one case
+        s[i]=s[j];
+        s[j]=temp;
+        i++; 
+        j--;
+        letMeReverse(s,i,j); // Recursive call 
+    }*/
+
+
